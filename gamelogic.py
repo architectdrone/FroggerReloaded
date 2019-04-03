ea44b9a51f7d19859a54461b03c1b9e8fb75cebe
Owen Mellema <owenmllm@gmail.com>
2019-04-03 09:24:55 -0500

    Start adapting gameLogic to work with board

--- gamelogic.py
+++ gamelogic.py
@@ -14,90 +14,33 @@ SCORE
 
 Number of LIVES
 '''
+FROGGER_INITIAL_X = 0
+FROGGER_INITIAL_Y = 0
+SIZE_x = 10
+SIZE_Y = 10
 
 myBoard = b.Board(1, 1)
 def initialize(x_size, y_size):
+    global myBoard, FROGGER_INITIAL_X, FROGGER_INITIAL_Y, frog_id
     '''
     Initializes game board
     @param x_size x dimension of board
     @param y_size y dimension of board
     '''
     myBoard = b.Board(x_size, y_size)
-
+    myBoard.addSubObject(frog_id, "frog", x = FROGGER_INITIAL_X, y = FROGGER_INITIAL_Y)
     #place subobjects
 
+next_id = 1 #This is the next available id. Everytime we add a new subobject, we increment this id, and use the one that was there. We start at 1 so as to reserve 0 for the frog.
+frog_id = 0 #This is the id of the frog. We must reserve just 0.
+obstacle_ids = [] #This is a list of ids representing things that are dangerous to frogger. If there is a collision with id 0 (frogger) and one of these, frogger is dead!
+platform_ids = [] #This is a list of ids representing things that are platforms.
 
-def Obstacle
-    
+initialize(SIZE_x, SIZE_Y)
 
-class Frog():
-    def __init__(self, x, y):
-        '''
-        Initializes frog object
-        @param x The current x coordinate of the frog
-        @param y The current y coordinate of the frog
-        @throw IndexError if requested coordinate is off the board.
-        '''
-        self.x_init = x #initial x coordinate
-        self.y_init = y #initial y coordinate
-    
-    def reset_pos(self):
-        '''
-        Resets the frog to initial position
-        @param self
-        '''
-        self.x = self.x_init
-        self.y = self.y_init
-    
-    def getCoordinate(x, y):
-        '''
-        Get the state of the current position of the frog, TODO
-        @param x The current x coordinate of the frog
-        @param y The current y coordinate of the frog
-        @throw IndexError if requested coordinate is off the board. 
-        @return state of the frog
-        '''
-        global myBoard
-        currentPos = myBoard.getXY(x, y) #This is already formatted as a dictionary
-        return currentPos
-
-    def move(self, x, y):
-        '''
-        Movement of frog, TODO
-        @param x The current x coordinate of the frog
-        @param y The current y coordinate of the frog
-        @throw IndexError if requested coordinate is off the board. 
-        @return update position of frog
-        '''
-        global myBoard
-        inRange = lambda x, y: (x >= 0) and (x < myBoard.x_size) and (y >= 0) and (y < myBoard.y_size)
-
-            def up():
-                self.y = self.y + 1
-            def down():
-                self.y = self.y - 1
-            def right():
-                self.x = self.x + 1
-            def left():
-                self.x = self.x - 1
-
-            #prevents frog from moving out of bounds
-            if self.x < 0: 
-                self.x = 0
-            if self.y < 0:
-                self.y = 0    
-
-        def interact()
-        '''
-        What happens when the frog encounters a subobject, TODO
-        @param x The current x coordinate of the frog
-        @param y The current y coordinate of the frog
-        '''
-
-
-
-def road() :
-    setLane(self, y, 'road')
-
-def river() :
-    setLane(self, y, 'river')
+def frogUp():
+    global myBoard, frog_id
+    theSubObject = myBoard.getSubObject(frog_id)
+    current_x = theSubObject['x']
+    current_y = theSubObject['y']
+    myBoard.editSubObject(frog_id, x = current_x, y = current_y+1)
