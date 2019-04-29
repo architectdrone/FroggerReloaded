e59ed263991fd1d16ccba5635a036260f43bf515
Gwen Liu <k579l290@ku.edu>
2019-04-29 16:12:06 -0500

    fix syntax error

--- highScores.py
+++ highScores.py
@@ -6,11 +6,11 @@ def readFromFileAndPrint(file_name):
     Reads entries from the score file and prints the entries
     @param file_name Name of file
     '''
-	file = open(file_name, 'r')
-	lines = file.readlines()
-	file.close
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    file.close
 
-	for line in lines:
+    for line in lines:
 	    entry = line.strip().split(",") #format in file will be name,score
 	    print(entry[0] , entry[1]) #entry[0] is name, entry[1] is score
         
@@ -21,8 +21,8 @@ def writeToFile(file_name, name, score):
     @param score User score to be added
     '''
     file = open(file_name, 'a')
-	file.write(name + ',' + score)
-	file.close()
+    file.write(name + ',' + score)
+    file.close()
 
 def SortAndCompareScores(file_name, user_score):
     '''
@@ -33,7 +33,7 @@ def SortAndCompareScores(file_name, user_score):
     highScore = False
     userScores_list = []
     file = open(file_name, 'r')
-	lines = file.readlines()
+    lines = file.readlines()
     for line in lines:
         entry = line.strip().split(",")
         score = int(entry[1])
@@ -42,8 +42,8 @@ def SortAndCompareScores(file_name, user_score):
     userScores_list.sort(reverse=True) #sort user scores descending order
     userScores_list = userScores_list[:10] #limit to top 10 entries
 
-    for i in range len(userScores_list):
-        if user_score >= userScores_list[i]:
+    for score in userScores_list:
+        if user_score >= score:
             highScore = True
     
     return highScore
