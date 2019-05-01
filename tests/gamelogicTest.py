0c5e6cdd3963c73f9e361f4ccc107de6caa56fe0
Gwen Liu <k579l290@ku.edu>
2019-05-01 02:11:00 -0500

    test

--- tests/gamelogicTest.py
+++ tests/gamelogicTest.py
@@ -1,3 +1,4 @@
+#test frog movement and score in game play
 import sys
 sys.path.append("..")
 
@@ -26,7 +27,6 @@ class gamelogicTest(unittest.TestCase):
         self.g.initialize()
         self.g.frogDown()
         assert 'type' in self.g.getXY(1,0) and self.g.getXY(1,0)['type'] is not None #Test if there is a subobject at the position.
-        print(self.g.getXY(1,0))
         self.assertEqual(self.g.getXY(1,0)['type'][0], 'frog') #Test if the subobject there is frog.
 
     def test_frogRight(self):
@@ -46,38 +46,13 @@ class gamelogicTest(unittest.TestCase):
         assert 'type' in self.g.getXY(0,1) and self.g.getXY(0,1)['type'] is not None #Test if there is a subobject at the position.
         self.assertEqual(self.g.getXY(0,1)['type'][0], 'frog') #Test if the subobject there is frog.
 
-
-    '''
     def test_score(self):
-        self.g.myBoard.addSubObject(0,"frog", 0, 9)
-        self.g.game.frogUp()
-        y = self.g.myBoard.getSubObject(0)['y']
-        self.assertEqual(self.g.score, 1)
-    
-    def test_frogIntersect(self):
-        self.g.myBoard.addSubObject(0,"frog", 1, 1)
-        self.g.myBoard.addSubObject(1,"testObject", 1, 1)
-        self.assertEqual(self.g.getFrogIntersect, 1)
-    
-    def test_getFrogCollisions(self):
-        self.g.myBoard.addSubObject(0,"frog", 1, 1)
-        self.g.myBoard.addSubObject(1,"testObject1", 1, 1)
-        self.g.myBoard.addSubObject(2,"testObject2", 1, 1)
-        self.assertEqual(self.g.getFrogCollisions, ["testObject1" , "testObject2"])
-    
-    def test_frogCheck(self): #incomplete
-        self.g.myBoard.addSubObject(0,"frog", 1, 1)
-        self.g.myBoard.addSubObject(1, "truck", 1, 1)
-        self.assertEqual(self.g.frogCheck, False)  
-    
-    TO-BE tested
-    Public functions:
-        initialize
-        update
-        getXY
-        
-    '''
-
+        self.assertEqual(self.g.score(), 0)
+        self.g.init_x = 0
+        self.g.init_y = 9
+        self.g.initialize() #set initial position to (0,9)
+        self.g.frogUp()
+        self.assertEqual(self.g.score(), 1) #score increments when moved up
 
 if __name__ == '__main__':
     unittest.main()
